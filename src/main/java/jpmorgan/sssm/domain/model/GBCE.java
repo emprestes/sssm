e5e12d61557df384aed4cbdd2d5d13790bdf66fb
Prestes, E. M <emprestes@gmail.com>
2016-10-14 12:19:23 +0100

    #1 Refactoring (calcVolumeWeighted).

--- src/main/java/jpmorgan/sssm/domain/model/GBCE.java
+++ src/main/java/jpmorgan/sssm/domain/model/GBCE.java
@@ -7,19 +7,18 @@ import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class GBCE {
 
     private final Set<Stock> stocks = new HashSet<>();
 
-    public static GBCE create() {
-        return new GBCE();
-    }
-
     private GBCE() {
         super();
+    }
+
+    public static GBCE create() {
+        return new GBCE();
     }
 
     public Set<Stock> getStocks() {
@@ -33,20 +32,16 @@ public class GBCE {
 
     public BigDecimal calcAllShareIndexes() {
         BigDecimal value;
-        Function<Stock, BigDecimal> f;
         long n;
 
-        f = Stock::calcVolumeWeighted;
         n = stocks.size();
-        value = BigDecimal.ZERO;
-
         value = stocks.stream()
                 .sorted((i1, i2) -> i1.getSymbol().compareToIgnoreCase(i2.getSymbol()))
                 .collect(Collectors.groupingBy(Stock::getSymbol))
                 .values().stream()
                 .map(list -> list.stream()
                         .map(Stock::calcVolumeWeighted)
-                        .findFirst().get())
+                        .reduce(BigDecimal.ZERO, (zero, first) -> first))
                 .reduce(BigDecimal.ONE, BigDecimal::multiply);
         value = Math.pow(value, n);
 
